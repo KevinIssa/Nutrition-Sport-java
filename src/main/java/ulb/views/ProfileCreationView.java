d7134966ad3a91bbbaafd8502d7a8d3e8e0b3700
LinSFa <kevin.vandervaeren@ulb.be>
2024-03-03 20:35:35 +0100

    Vieuw: User profile data -> store in view variable + refactore of ViewControler emplacement
    co-authored-by: Tran Hà Uyên <ha.tran@ulb.be>

---
+++ src/main/java/ulb/views/ProfileCreationView.java
@@ -0,0 +1,58 @@
+package ulb.views;
+
+import javafx.scene.control.RadioButton;
+import ulb.controllers.AbstractController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+public class ProfileCreationView extends AbstractController implements Initializable {
+    @FXML
+    private TextField firstname;
+    @FXML
+    private TextField surname;
+    @FXML
+    private DatePicker birthdate;
+    @FXML
+    private TextField height;
+    @FXML
+    private TextField weight;
+    @FXML
+    private ToggleGroup sex; // radio button
+
+    System.Logger logger = System.getLogger(ProfileCreationView.class.getName());
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        logger.log(System.Logger.Level.INFO, "ProfileCreationView initialized");
+        this.birthdate.setValue(LocalDate.now());
+    }
+
+    public void saveProfile() {
+        String savedSurname = this.surname.getText();
+        String savedFirstname = this.firstname.getText();
+        LocalDate selectedDate = this.birthdate.getValue();
+        int day = selectedDate.getDayOfMonth();
+        int month = selectedDate.getMonthValue();
+        int year = selectedDate.getYear();
+
+        String savedHeight = this.height.getText();
+        String savedWeight = this.weight.getText();
+        try {
+            Integer.parseInt(savedHeight);
+            Integer.parseInt(savedWeight);
+        } catch (NumberFormatException e) {
+            logger.log(System.Logger.Level.ERROR, "height and weight must be numbers");
+            return;
+        }
+        RadioButton selectedRadioButton = (RadioButton) sex.getSelectedToggle();
+        String savedSex = selectedRadioButton.getText();
+        logger.log(System.Logger.Level.INFO, "surname: " + savedSurname + " firstname: " + savedFirstname + " birthdate: " + day + "/" + month + "/" + year + " height: " + savedHeight + " weight: " + savedWeight +" sex:" + savedSex);
+    }
+}
