cfd36e738b4a8df90bfba7fc46aaec40c317142e
hugocharels <hugocharels@hotmail.com>
2024-04-20 11:50:31 +0200

    :boom: new repositories

---
+++ src/main/java/ulb/repositories/JSONComposeMealRepository.java
@@ -0,0 +1,30 @@
+/*
+ * Ce projet est une application de santé et de bien-être développée dans le cadre du cours INFO-F-307 à l'ULB.
+ *
+ * Groupe : 06
+ * Étudiants :
+ * - Kevin ISSA
+ * - Hamza CAEYMAN
+ * - Alexandru MELNIC
+ * - Ze-Xuan XU
+ * - Bao TRAN
+ * - Hà Uyên TRAN
+ * - Hugo CHARELS
+ * - Hodo SOULEIMAN AHMED
+ * - Kevin VANDERVAEREN
+ * - Arthur INSTALLÉ
+ *
+ * Date : 2024
+ */
+package ulb.repositories;
+
+import ulb.models.Meal;
+
+public class JSONComposeMealRepository extends JSONRepository<Meal>
+		implements ComposeMealRepository {
+
+	@Override
+	protected Class<Meal> getObjectType() {
+		return Meal.class;
+	}
+}
---
+++ src/main/java/ulb/repositories/JSONConsumedMealRepository.java
@@ -0,0 +1,30 @@
+/*
+ * Ce projet est une application de santé et de bien-être développée dans le cadre du cours INFO-F-307 à l'ULB.
+ *
+ * Groupe : 06
+ * Étudiants :
+ * - Kevin ISSA
+ * - Hamza CAEYMAN
+ * - Alexandru MELNIC
+ * - Ze-Xuan XU
+ * - Bao TRAN
+ * - Hà Uyên TRAN
+ * - Hugo CHARELS
+ * - Hodo SOULEIMAN AHMED
+ * - Kevin VANDERVAEREN
+ * - Arthur INSTALLÉ
+ *
+ * Date : 2024
+ */
+package ulb.repositories;
+
+import ulb.models.ConsumedMeal;
+
+public class JSONConsumedMealRepository extends JSONRepository<ConsumedMeal>
+		implements ConsumedMealRepository {
+
+	@Override
+	protected Class<ConsumedMeal> getObjectType() {
+		return ConsumedMeal.class;
+	}
+}
