a0528c1c5ccbe960892e1e4de647d32ea333c56e
KevinIssa <kevin.issa@ulb.be>
2024-05-06 16:02:15 +0200

    Refactor historyMeal

--- src/main/java/ulb/controllers/MealHistoryController.java
+++ src/main/java/ulb/controllers/MealHistoryController.java
@@ -72,22 +72,9 @@ public class MealHistoryController extends AppController
 	}
 
 	@Override
-	public void deleteFood(HBox foodBox) {
-		String dateString = ((Label) foodBox.getChildren().get(4)).getText();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy 'à' HH:mm");
-		// Parse the string into a LocalDateTime object
-		LocalDateTime dateTime = LocalDateTime.parse(dateString, formatter);
-		this.consumeMealService.deleteConsumedFood(toConsumedFoodDTO(foodBox), dateTime);
-	}
+	public void deleteFood(ConsumedFoodDTO consumedFood, LocalDateTime dateTime) {
 
-	private ConsumedFoodDTO toConsumedFoodDTO(HBox foodBox) {
-		String name = ((Label) foodBox.getChildren().get(0)).getText();
-		int quantity =
-				Integer.parseInt(((Label) foodBox.getChildren().get(2)).getText().split(" ")[0]);
-		int calories =
-				Integer.parseInt(((Label) foodBox.getChildren().get(6)).getText().split(" ")[0]);
-		String unit = ((Label) foodBox.getChildren().get(2)).getText().split(" ")[1];
-		return new ConsumedFoodDTO(name, quantity, calories, unit);
+		this.consumeMealService.deleteConsumedFood(consumedFood, dateTime);
 	}
 
 	/**
